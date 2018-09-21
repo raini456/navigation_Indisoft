534ced125791353ec4413ec7e212b985b6ba8a2f
Teilnehmer <Teilnehmer@172.16.18.22>
2018-09-21 09:17:19 +0200

    3rd commit 21.09.2018

--- public_html/assets/js/offCanvasMainJS.js
+++ public_html/assets/js/offCanvasMainJS.js
@@ -10,18 +10,14 @@ $(document).ready(function () {
     //var navToggle =['<div id="hamburger">&#127828;</div>'].join("");
     var navToggle = ['<div id="hamburger">&#9776; </div>'].join("");
     $("nav").prepend(navToggle);
+    
 
     $('#hamburger').click(function () {
-        $('.js nav #site_nav').slideDown();        
+        $('.js nav #site_nav').toggle('slide');        
     });
     $('.js nav #site_nav li').click(function(){              
         if(width<655){
-          $('.js nav #site_nav').slideUp();
+          $('.js nav #site_nav').toggle('slide');
         }
-    })
-    $('#hamburger').sidr({
-        side:'left',
-        name:'site_nav'
-    });
-    
+    })   
 });
